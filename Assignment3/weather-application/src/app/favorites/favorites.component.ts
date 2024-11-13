b3e862cb417b3affabb00e86129c83c912438706
Anthony Carreon <anthonycarreon@zoho.com>
2024-11-13 13:48:09 -0800

    going to work on the detail rows an aminiation

--- Assignment3/weather-application/src/app/favorites/favorites.component.ts
+++ Assignment3/weather-application/src/app/favorites/favorites.component.ts
@@ -10,17 +10,22 @@ import { CustomerResultsService } from '../customer-results.service';
 export class FavoritesComponent implements OnInit {
   @Input() city?: string;
   @Input() state?: string;
+  alreadyLoaded = false; 
   row: any[] = [];
   constructor(
     private customerService: CustomerResultsService,
   ) { 
   } 
   ngOnInit(): void {
+    console.log('Inside ngOninIt')
+    console.log('this.alreadyLoaded', this.alreadyLoaded);
     this.customerService.getFavorites().subscribe((response: any) =>{
       this.row = response
     });
+    this.alreadyLoaded = true;
   }
   removeFavorite(latitude: number, longitude: number, city: string, state: string){
+    console.log('Inside removeFavortie')
     this.customerService.removeFavoriteRow(latitude, longitude, city, state).subscribe({});
     this.row = [];
     this.customerService.getFavorites().subscribe((response: any) =>{
